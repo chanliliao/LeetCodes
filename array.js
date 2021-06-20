1f31d34011e46dd54b789bd570f05b3d8396a942
Chan Li Liao <cliao119@gmail.com>
2021-06-19 22:27:54 -0400

    arrarys

--- array.js
+++ array.js
@@ -221,4 +221,39 @@ let productExceptSelf3 = (nums) => {
   return nums;
 };
 
-console.log(productExceptSelf([1, 2, 3, 4]));
+// console.log(productExceptSelf([1, 2, 3, 4]));
+
+// Question 152
+// Maximum Product Subarray
+// Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+// It is guaranteed that the answer will fit in a 32-bit integer.
+// A subarray is a contiguous subsequence of the array.
+// Example 1:
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+// Example 2:
+// Input: nums = [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+// Constraints:
+// 1 <= nums.length <= 2 * 104
+// -10 <= nums[i] <= 10
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+let maxProduct = (nums) => {
+  let max = -Infinity;
+  let currentMax = 1;
+  let currentMin = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    let prevMax = currentMax;
+    currentMax = Math.max(nums[i], prevMax * nums[i], currentMin * nums[i]);
+    currentMin = Math.min(nums[i], prevMax * nums[i], currentMin * nums[i]);
+    max = Math.max(currentMax, max);
+  }
+
+  return max;
+};
+
+console.log(maxProduct([2, 3, -2, 4, 6, -2, -2]));
