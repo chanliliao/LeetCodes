36cf884cbb445825ded528a3da12f544096c8eea
Chan Li Liao <cliao119@gmail.com>
2021-06-24 20:47:13 -0400

    binary

--- binary.js
+++ binary.js
@@ -78,6 +78,43 @@ let reverseBits1 = (n) => {
 
 //console.log(reverseBits1('00000010100101000001111010011100'));
 
+// Q191
+// Num of 1 Bits
+// Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+// Note:
+// Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+// In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+// Example 1:
+// Input: n = 00000000000000000000000000001011
+// Output: 3
+// Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+// Example 2:
+// Input: n = 00000000000000000000000010000000
+// Output: 1
+// Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+// Example 3:
+// Input: n = 11111111111111111111111111111101
+// Output: 31
+// Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+
+let hammingWeight = (n) => {
+  let sum = 0;
+
+  while (n != 0) {
+    sum += n & 1;
+    n = n >>> 1;
+  }
+
+  return sum;
+};
+
+let hammingWeight2 = (n) => {
+  // remove 0s from base2 representation of the number
+  return n.toString(2).replace(/0/g, '').length;
+};
+
+console.log(hammingWeight2(00000000000000000000000000001011));
+
 // Q371
 // sum of two integers
 // Given two integers a and b, return the sum of the two integers without using the operators + and -.
