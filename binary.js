d7f363670e7815867bd4c14ff87fa976bcb391d0
Chan Li Liao <cliao119@gmail.com>
2021-06-26 00:00:03 -0400

    .

--- binary.js
+++ binary.js
@@ -115,6 +115,43 @@ let hammingWeight2 = (n) => {
 
 console.log(hammingWeight2(00000000000000000000000000001011));
 
+// Q338
+// Counting bits
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+// Example 1:
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// Example 2:
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+// Constraints:
+// 0 <= n <= 105
+// Follow up:
+// It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+
+let countBit = (n) => {
+  let res = [0];
+  for (let i = 1; i <= num; i++) {
+    const half = i >> 1;
+    const odd = i & 1;
+    res[i] = res[half] + odd;
+  }
+  return res;
+};
+
+console.log(countBit(5));
+
 // Q371
 // sum of two integers
 // Given two integers a and b, return the sum of the two integers without using the operators + and -.
