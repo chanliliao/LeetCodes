31f6bbd8bc057a835932a29841a6533782847b94
Chan Li Liao <cliao119@gmail.com>
2021-07-28 01:41:44 -0400

    dp

--- dp.js
+++ dp.js
@@ -156,3 +156,243 @@ const coinChange = (coins, amount) => {
   }
   return dp[amount] === Infinity ? -1 : dp[amount];
 };
+
+// Q300
+// Longest incraseing subsquence
+// Given an integer array nums, return the length of the longest strictly increasing subsequence.
+// A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+// Example 1:
+// Input: nums = [10,9,2,5,3,7,101,18]
+// Output: 4
+// Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+// Example 2:
+// Input: nums = [0,1,0,3,2,3]
+// Output: 4
+// Example 3:
+// Input: nums = [7,7,7,7,7,7,7]
+// Output: 1
+
+var lengthOfLIS = function (nums) {
+  const dp = Array.from(nums, () => 1);
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
+
+// Q 139 
+// Word Break
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+// Example 1:
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+// Example 2:
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+// Example 3:
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: false
+
+const wordBreak = (s, wordDict) => {
+  if (wordDict == null || wordDict.length === 0) return false;
+
+  const set = new Set(wordDict);
+  const dp = Array(s.length + 1).fill(false);
+  dp[0] = true;
+
+  for (let end = 1; end <= s.length; end++) {
+    for (let start = 0; start < end; start++) {
+      const w = s.slice(start, end);
+      if (dp[start] === true && set.has(w)) {
+        dp[end] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+};
+
+// Q198 
+// house robber
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+// Example 2:
+// Input: nums = [2,7,9,3,1]
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+// Total amount you can rob = 2 + 9 + 1 = 12.
+
+var rob2 = function(nums) {
+  let memo = [];
+  memo[0] = 0;
+  memo[1] = nums[0];
+  
+  for (let i=1;i<nums.length;i++) {
+      memo[i+1] = Math.max(memo[i], memo[i-1] + nums[i]);
+  }
+  
+  return memo[nums.length];   
+}
+
+// Q213 
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+// Example 1:
+// Input: nums = [2,3,2]
+// Output: 3
+// Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+// Example 2:
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+// Example 3:
+// Input: nums = [0]
+// Output: 0
+
+// the idea is to get two dp array one with the first house and one with the last house
+var rob = function(nums) {
+  let n = nums.length;
+  if(n===0) return 0;
+  if(n===1) return nums[0];
+  if(n===2) return Math.max(nums[0],nums[1])
+  
+  let dp1 = new Array(n);
+  let dp2 = new Array(n);
+  
+  computeResult(0,n-2,dp1,nums);
+  computeResult(1,n-1,dp2,nums);
+  
+  function computeResult(i,n,dp,nums){
+      dp[i] = nums[i]
+      dp[i+1] = Math.max(dp[i],nums[i+1])
+      
+      for(let j=i+2; j<=n; j++){
+          dp[j] = Math.max(dp[j-1],dp[j-2]+nums[j])
+      }
+  }
+  return Math.max(dp1[n-2],dp2[n-1])
+};
+
+// Q55
+// jump game
+// Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+// Each element in the array represents your maximum jump length at that position.
+// Determine if you are able to reach the last index.
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+// Input: nums = [3,2,1,0,4]
+// Output: false
+// Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+
+var canJump = function(nums) {
+  let max = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+      if (i > max) return false;
+      
+      if (i + nums[i] >= nums.length - 1) return true;
+          
+      max = Math.max(max, i + nums[i]);
+  }
+};
+
+//Q337 
+// Combination sum IV
+// Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+// The answer is guaranteed to fit in a 32-bit integer.
+// Example 1:
+// Input: nums = [1,2,3], target = 4
+// Output: 7
+// Explanation:
+// The possible combination ways are:
+// (1, 1, 1, 1)
+// (1, 1, 2)
+// (1, 2, 1)
+// (1, 3)
+// (2, 1, 1)
+// (2, 2)
+// (3, 1)
+// Note that different sequences are counted as different combinations.
+// Example 2:
+// Input: nums = [9], target = 3
+// Output: 0
+
+// TC = O(mn); SC = O(m)
+var combinationSum4 = function(nums, target) {
+  const dp = Array(target + 1).fill(0);
+  dp[0] = 1;
+  
+// for each target sum, find the number of combinations possible
+  for(let i = 1; i <= target; ++i) {
+      nums.forEach(num => {
+          if(i >= num) {
+              dp[i] += dp[i - num];
+          }
+      })
+  }
+  return dp[target];
+};
+
+// Q91 
+// decode ways
+// A message containing letters from A-Z can be encoded into numbers using the following mapping:
+// 'A' -> "1"
+// 'B' -> "2"
+// ...
+// 'Z' -> "26"
+// To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+// "AAJF" with the grouping (1 1 10 6)
+// "KJF" with the grouping (11 10 6)
+// Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+// Given a string s containing only digits, return the number of ways to decode it.
+// The answer is guaranteed to fit in a 32-bit integer.
+// Example 1:
+// Input: s = "12"
+// Output: 2
+// Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+// Example 2:
+// Input: s = "226"
+// Output: 3
+// Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+// Example 3:
+// Input: s = "0"
+// Output: 0
+// Explanation: There is no character that is mapped to a number starting with 0.
+// The only valid mappings with 0 are 'J' -> "10" and 'T' -> "20", neither of which start with 0.
+// Hence, there are no valid ways to decode this since all digits need to be mapped.
+// Example 4:
+// Input: s = "06"
+// Output: 0
+// Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+
+const numDecodings = (s) => {
+  const dp = Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= s.length; i++) {
+    const one = Number(s.slice(i - 1, i)); // '0' to '9'
+    const two = Number(s.slice(i - 2, i)); // '00' to '99'
+    if (one !== 0) dp[i] += dp[i - 1]; // We want [1, 9]
+    if (10 <= two && two <= 26) dp[i] += dp[i - 2]; // We want [10, 26]
+  }
+  return dp[s.length];
+};
