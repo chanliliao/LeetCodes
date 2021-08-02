75bd3334e4c96ef3ed3ddc0a06a48552e4b207fd
Chan Li Liao <cliao119@gmail.com>
2021-08-01 20:26:49 -0400

    graph

--- graph.js
+++ graph.js
@@ -241,3 +241,36 @@ const findMaxGold = (grid, i, j, rows, cols, gold = 0) => {
     return gold;
   }
 };
+
+// Q797
+// all path from source to target
+// Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+// The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+// Example 1:
+// Input: graph = [[1,2],[3],[3],[]]
+// Output: [[0,1,3],[0,2,3]]
+// Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+var allPathsSourceTarget = function (graph) {
+  const target = graph.length - 1;
+
+  const res = [];
+
+  const DFS = (node, path) => {
+    path.push(node);
+
+    // if we've reached the target, we've found a path
+    if (node === target) {
+      res.push(path);
+      return;
+    }
+
+    for (let edge of graph[node]) {
+      DFS(edge, [...path]);
+    }
+  };
+
+  DFS(0, []);
+
+  return res;
+};
