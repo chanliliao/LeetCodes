c17322e83975bcbaf932302f244d94633de8fe29
Chan Li Liao <cliao119@gmail.com>
2021-08-29 01:15:17 -0400

    python graph

--- graph.py
+++ graph.py
@@ -50,3 +50,37 @@ def canFinish(numCourses, prereq):
     if not dfs(crs):
       return False
   return True
+
+# Q417
+# course schedule
+def pacificAtlantic(heights):
+  # define rows and cols and the sets 
+  ROW,COL = len(heights), len(heights[0])
+  pac, atl = set(), set()
+
+  # dfs
+  def dfs(r,c,visited,prevHeight):
+    if(r,c) in visited or r < 0 or c < 0 or r == ROW or c == COL or heights[r][c] < prevHeight:
+      return
+    visited.add((r,c))
+    dfs(r-1,c,visited,heights[r][c])
+    dfs(r,c-1,visited,heights[r][c])
+    dfs(r+1,c,visited,heights[r][c])
+    dfs(r,c+1,visited,heights[r][c])
+
+  # loop through the graph for col
+  for c in range(COL):
+    dfs(0,c,pac,heights[0][c])
+    dfs(ROW -1,c,atl,heights[ROW-1][c])
+  # loop through the graph for row
+  for r in range(ROW):
+    dfs(r,0,pac,heights[r][0])
+    dfs(r,COL-1,atl,heights[r][COL-1])
+
+  # loop through graph again to see intersection and add to result
+  res = []
+  for r in range(ROW):
+    for c in range(COL):
+      if (r,c) in pac and (r,c) in atl:
+        res.append([r,c])
+  return res
